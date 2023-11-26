c4705352aa5b943e9485801f0b2ebfacf8f1b2c6
Dmytro Liuba <lyubadmitry2@gmail.com>
2023-11-26 13:06:47 +0200

    update

--- src/js/handleForm.js
+++ src/js/handleForm.js
@@ -3,10 +3,12 @@ import { fetchProduct } from "../requests/products";
 import { createProductMarkup } from "../services/markupService";
 
 export async function handleForm(event) {
-    event.preventDefault();
-
-    const id = event.currentTarget.elements.id.value;
-
-    const data = await fetchProduct(id);
-    refs.singleProduct.innerHTML = createProductMarkup(data)
+    try {
+        event.preventDefault();
+        const id = event.currentTarget.elements.id.value;
+        const data = await fetchProduct(id);
+        refs.singleProduct.innerHTML = createProductMarkup(data);
+    } catch (error) {
+        console.log(error);
+    }
 }
